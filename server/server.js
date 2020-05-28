b6b2637469a2b8e89a8440780ad7d36268e71125
EDUARDO LIMA <43394450+eng-edu@users.noreply.github.com>
2020-05-28 15:46:17 -0300

    UP

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const http = require('http');
 
 
 //define a porta
-const port = normalizePort(process.env.port||'7000');
+const port = normalizePort(process.env.PORT||'7000');
 app.set('port', port);
 
 //cria o server
