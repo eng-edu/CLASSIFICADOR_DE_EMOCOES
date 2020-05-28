beb1143fb409a08195018d3ee120d3216d0861a2
EDUARDO LIMA <43394450+eng-edu@users.noreply.github.com>
2020-05-28 15:25:16 -0300

    up

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bayes = require('./algoritimos/bayes');
 
+app.get('/', function(req, res){
+  res.send("API ONLINE!")
+})
+
 app.get('/analize/:frase', function (req, res) {
   var frase = req.params.frase;
   bayes.prediction(frase, function (x) {
